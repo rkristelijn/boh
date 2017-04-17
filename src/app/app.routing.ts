274045a714dc43c6a86facd90478ee8dbb79e0f9
Remi Kristelijn <remi.kristelijn@accenture.com>
2017-04-17 08:16:25 +0200

    fixed getting error 404 when refreshing by hashstrategy

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -40,4 +40,4 @@ const appRoutes: Routes = [
   }
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, {useHash: true});
